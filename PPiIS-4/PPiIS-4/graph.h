51de99b72df11b0b904ae234e0890a0dae1ea215
Антон Клинцов <92170048@study.bsuir.by>
2020-12-11 20:37:53 +0300

    set_print

--- PPiIS-4/PPiIS-4/graph.h
+++ PPiIS-4/PPiIS-4/graph.h
@@ -25,7 +25,6 @@ private:
 	T _data;
 };
 
-
 class edge
 {
 public:
@@ -65,19 +64,21 @@ template <typename T>
 class graph
 {
 public:
-	graph() :
-	{	}
+	graph(void) {};
+	~graph(void) {};
 
 
-	void delete_vertex(const T& data);
-	void add_vertex(const T& data);
-	int find_vertex_data(const T& data);
+
+	void add_vertex(const T& data);								//Addition
+	int find_vertex(const T& data);								//Search by value
 
 	void add_edge(const int& from, const int& to);				//Addition by number
 	void add_edge(const T& from, const T& to);					//Addition by value
 	bool find_edge(const int& from, const int& to);				//Search by number
 	int find_edge(const T& from, const T& to);					//Search by value
-
+	void delete_edge(const int& from, const int& to);			//Deletion by number
+	void delete_edge(const T& from, const T& to);				//Deletion by value
+	void print_graph();											//Show graph
 
 private:
 	vector<vertex<T>> _vertexes;
@@ -85,30 +86,25 @@ private:
 };
 
 
-template<typename T>
-inline void graph<T>::delete_vertex(const T& data)
-{
-	//...
-}
-
+//Addition
 template<typename T>
 inline void graph<T>::add_vertex(const T& data)
 {
-	if (find_vertex(data) == false)
+	if (find_vertex(data) == -1)
 		_vertexes.push_back(vertex<T>(data));
 	else
 		cout << "Math founded!" << endl;
 }
 
+//Search by value
 template<typename T>
-inline int graph<T>::find_vertex_data(const T& data)
+inline int graph<T>::find_vertex(const T& data)
 {
 	for (int i = 0; i < _vertexes.size(); ++i)
 		if (_vertexes[i].get_data() == data)
 			return i;
 	return -1;
 }
-
 
 //Addition by number
 template<typename T>
@@ -118,14 +114,15 @@ inline void graph<T>::add_edge(const int& from, const int& to)
 		_edges.push_back(edge(from, to));
 }
 
+//Addition by value
 template<typename T>
 inline void graph<T>::add_edge(const T& from, const T& to)
 {
 	int first = find_vertex_data(from), sec = find_vertex_data(to);
 	if (first != -1 && sec != -1 && find_edge(first, sec) == false)
+		_edges.push_back(edge(find_vertex(from), find_vertex(to)));
 
 }
-
 
 //Search by number
 template<typename T>
@@ -137,7 +134,6 @@ inline bool graph<T>::find_edge(const int& from, const int& to)
 	return false;
 }
 
-
 //Search by value
 template<typename T>
 inline int graph<T>::find_edge(const T& from, const T& to)
@@ -146,6 +142,38 @@ inline int graph<T>::find_edge(const T& from, const T& to)
 		if (_vertexes[_edges[i].get_from()].get_data() == from && _vertexes[_edges[i].get_to()].get_data() == to)
 			return i;
 	return -1;
+}
+
+//Deletion by number
+template<typename T>
+inline void graph<T>::delete_edge(const int& from, const int& to)
+{
+	for (int i = 0; i < _edges.size(); ++i)
+		if (find_edge(from, to) == true)
+		{
+			_edges.erase(_edges.begin() + i);
+			for (int k = i; k < _edges.size(); ++k)
+				_edges[k] = _edges[k + 1];
+			if (_edges.size() != 0)
+				_edges.pop_back();
+		}
+}
+
+//Deletion by value
+template<typename T>
+inline void graph<T>::delete_edge(const T& from, const T& to)
+{
+	for (int i = 0; i < _edges.size(); ++i)
+		if (find_edge(from, to) != -1)
+			_edges.erase(_edges.begin() + i);
+}
+
+//Show graph
+template<typename T>
+inline void graph<T>::print_graph()
+{
+	for (int i = 0; i < _edges.size() : ++i)
+		cout << _vertexes[_edges[i].get_from()].get_data() << " ---> " << _vertexes[_edges[i].get_to()].get_data() << endl;
 }
 
 
