7df1405263ca572fd1e826d5bce48bf2ebfde272
Антон Клинцов <92170048@study.bsuir.by>
2020-12-11 18:19:02 +0300

    set_struct_edge

--- PPiIS-4/PPiIS-4/graph.h
+++ PPiIS-4/PPiIS-4/graph.h
@@ -1,15 +1,30 @@
 #pragma once
 #include <iostream>
 
+template <typename T>
 struct vertex
 {
-	int node;
+	T _data;
 
 	vertex() :
-		node(0)
+		_data(0)
 	{	}
 
-	vertex(const int& node) :
-		node(node)
+	vertex(const T& node) :
+		_data(node)
+	{	}
+};
+
+template <typename T>
+struct edge
+{
+	T _first, _sec;
+	edge() :
+		_first(0),
+		_sec(0)
+	{	}
+	edge(const T& first, const T& sec) :
+		_first(first),
+		_sec(sec)
 	{	}
 };
